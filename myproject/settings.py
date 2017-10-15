71045ed562d83c129510c247a18a5f010d2f8429
jrrwebdev <jrrwebdev@gmail.com>
2017-10-15 11:19:31 -0200

    Fix home and base.html

--- myproject/settings.py
+++ myproject/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,14 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 #
+
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-      os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT = '/home/ricardo/Dev/c2b'
-# os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
