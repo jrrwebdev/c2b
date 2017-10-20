d301217f76cff6830c31ff781762c63c9aad2684
jrrwebdev <jrrwebdev@gmail.com>
2017-10-20 12:02:30 -0200

    Update show buyevent main page

--- core/models.py
+++ core/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     def __str__(self):
         return self.description
 
+    def get_fields(self):
+        return [(field, field.value_to_string(self)) for field in Category._meta.fields]
+
 
 class BuyEvent(models.Model):
     """Class Buy Event - This class is to save data from Buy Events."""
